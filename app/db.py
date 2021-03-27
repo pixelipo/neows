cd4ee26de2dfecf28f19a0f3c80aefa878ba5f87
Marin Treselj <marin.treselj@forlagshuset.no>
2021-03-27 14:05:11 +0100

    First working release; crawler is fetching data from source and importing into DB

---
+++ app/db.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+
+def initDb(dbName):
+    conn = sqlite3.connect(dbName)
+
+    ## Do some setup
+    # cur = conn.cursor()
+    # cur.executescript('''
+    # DROP TABLE IF EXISTS Object;
+    # DROP TABLE IF EXISTS Approach;
+    #
+    # CREATE TABLE Object (
+    #     id     INTEGER NOT NULL PRIMARY KEY UNIQUE,
+    #     name   TEXT UNIQUE,
+    #     hazardous INTEGER,
+    #     diameter_min REAL,
+    #     diameter_max REAL
+    # );
+    #
+    # CREATE TABLE Approach (
+    #     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    #     date INTEGER,
+    #     miss REAL,
+    #     object_id INTEGER NOT NULL,
+    #     FOREIGN KEY(object_id) REFERENCES Object(id)
+    # );
+    # ''')
+
+    print("Database initialized")
+
+    return(conn)
+
+def insertObj(cur, id, name, hazardous, diameter_min, diameter_max):
+    return cur.execute(
+        '''INSERT OR IGNORE INTO Object (id, name, hazardous, diameter_min, diameter_max) VALUES ( ?, ?, ?, ?, ? )''',
+        ( id, name, hazardous, diameter_min, diameter_max )
+    )
+
+def insertApproach(cur, date, miss, object_id):
+    return cur.execute(
+        '''INSERT OR IGNORE INTO Approach (date, miss, object_id) VALUES ( ?, ?, ? )''',
+        ( date, miss, object_id )
+    )
--- main.py
+++ main.py
@@ -8,9 +8,12 @@ import json
 import sqlite3
 
 
+from app import db
+
 # init vars
 api_key = 'DEMO_KEY' # Fallback api_key if env folder configuration is missing
-page = '1'
+page = 1
+total = 2
 
 # fetch secrets from local file
 if os.path.isfile('env/conf.py'):
@@ -26,17 +29,18 @@ service = input("Select service([1] Browse [2] Lookup [3] Feed):")
 ## Browse data
 if (service == '1'):
     # example: https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY
-    parms['page'] = page
+    parms['page'] = str(page)
+    size = '20'
     baseUrl = 'https://api.nasa.gov/neo/rest/v1/neo/browse?'
-    print('Listing last', size, 'items:')
+    print('Listing last', size, 'items from page', parms['page'])
 
 
 ## Lookup NEO object
 if (service == '2'):
     # example: https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=DEMO_KEY
-    baseUrl = 'https://api.nasa.gov/neo/rest/v1/neo/?'
-    parms['neoId'] = '3542519'
-    print("Looking up object", parms['neoId'])
+    baseUrl = 'https://api.nasa.gov/neo/rest/v1/neo/2003200/?'
+    neoId = '2003200'
+    print("Looking up object", 'neoId')
 
 ## Get data feed
 if (service == '3'):
@@ -50,18 +54,55 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = baseUrl + urllib.parse.urlencode(parms)
-print(url)
-uh = urllib.request.urlopen(url, context=ctx)
-data = uh.read().decode()
+while page <= total:
+    url = baseUrl + urllib.parse.urlencode(parms)
+    print(url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
 
-# fetch remaining rate limit from response headers
-print('Requests remaining:', uh.info()['X-RateLimit-Remaining'])
+    # fetch remaining rate limit from response headers
+    remaining = uh.info()['X-RateLimit-Remaining']
+    print('Requests remaining:', remaining)
+    if (int(remaining) == 0) : break
 
-print('Retrieved', len(data), 'characters')
-js = json.loads(data)
+    print('Retrieved', len(data), 'characters')
 
-# print(js['links'])
+    try:
+        js = json.loads(data)
+        parms['page'] = str(int(parms['page']) + 1)
+        total = int(js['page']['total_pages'])
+    except:
+        print(data)  # We print in case unicode causes an error
 
-for neo in js['near_earth_objects']:
-    print(neo['name'], "is potentially hazardous:", neo['is_potentially_hazardous_asteroid'])
+    # print(js['links'])
+    conn = db.initDb('neows.sqlite')
+    cur = conn.cursor()
+
+
+    for i, neo in enumerate(js['near_earth_objects']):
+        db.insertObj(
+            cur=cur,
+            id=neo['id'],
+            name=neo['name'],
+            hazardous=neo['is_potentially_hazardous_asteroid'],
+            diameter_min=neo['estimated_diameter']['kilometers']['estimated_diameter_min'],
+            diameter_max=neo['estimated_diameter']['kilometers']['estimated_diameter_max']
+        )
+
+        conn.commit()
+
+        for cad in neo['close_approach_data']:
+            db.insertApproach(
+                cur=cur,
+                date=cad['epoch_date_close_approach'],
+                miss=cad['miss_distance']['kilometers'],
+                object_id=neo['id']
+            )
+
+        # if i == len(js['near_earth_objects']) :
+        #     print('Pausing for a bit...')
+        #     time.sleep(5)
+        # # neo_raw = new NeoRaw()
+        # print(neo['name'], "is potentially hazardous:", neo['is_potentially_hazardous_asteroid'])
+
+        # SELECT DISTINCT Object.name, Object.hazardous, Approach.miss FROM Object JOIN Approach ON Object.id = Approach.object_id ORDER by Approach.miss DESC LIMIT 10;
